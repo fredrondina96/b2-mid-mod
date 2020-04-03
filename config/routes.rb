088a852ca282eb2e72e87b83df295b30f34bb81f
Fred Rondina <fredrondina96@gmail.com>
2020-04-03 11:25:32 -0600

    Completed User story 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get "/mechanics", to: 'mechanics#index'
   get "/mechanics/:id", to: 'mechanics#show'
+  patch "/mechanics/:id", to: 'mechanics#update'
 
   get "/amusmant_parks/:park_id", to: 'parks#show'
 end
--- spec/features/mechanics/mechanic_show_page_spec.rb
+++ spec/features/mechanics/mechanic_show_page_spec.rb
@@ -28,13 +28,27 @@ RSpec.describe "As a user when I visit a mechanics show page", type: :feature do
     expect(page).to have_content(kumba.name)
 
   end
-end
 
-# Story 3
-# As a user,
-# When I go to a mechanics show page
-# I see their name, years of experience, and names of all rides they’re working on
-# And I also see a form to add a ride to their workload
-# When I fill in that field with an id of an existing ride and hit submit
-# I’m taken back to that mechanics show page
-# And I see the name of that newly added ride on this mechanics show page
+  it "has a form where I can add existing rides to mechanics workload" do
+    jimbo = Mechanic.create!(name: "Jimbo", years_of_expierence: 13)
+
+    busch_gardens = Park.create!(name: "Busch Gardens", admission_price: 50)
+
+    montu = busch_gardens.rides.create!(name: "Montu", thrill_rating: 8)
+    kumba = busch_gardens.rides.create!(name: "Kumba", thrill_rating: 7)
+
+    jimbo.rides <<  montu
+
+    visit "/mechanics/#{jimbo.id}"
+
+    expect(page).to have_content(montu.name)
+    expect(page).to_not have_content(kumba.name)
+
+    fill_in "ride_id", with: "#{kumba.id}"
+    click_button "Add Ride"
+
+    expect(current_path).to eq("/mechanics/#{jimbo.id}")
+    expect(page).to have_content(kumba.name)
+    expect(page).to have_content(montu.name)
+  end
+end
