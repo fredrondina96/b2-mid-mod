31c23cf5d15b307442e9bfb3cf2ad5975dec0440
Fred Rondina <fredrondina96@gmail.com>
2020-04-03 11:16:14 -0600

    Completed 1/2 pf user story 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "/mechanics", to: 'mechanics#index'
+  get "/mechanics/:id", to: 'mechanics#show'
 
   get "/amusmant_parks/:park_id", to: 'parks#show'
 end
--- spec/features/mechanics/mechanic_show_page_spec.rb
+++ spec/features/mechanics/mechanic_show_page_spec.rb
@@ -1,7 +1,31 @@
 require "rails_helper"
 
 RSpec.describe "As a user when I visit a mechanics show page", type: :feature do
-  it "text" do
+  it "should display a mechanics name and years of experience" do
+    jimbo = Mechanic.create!(name: "Jimbo", years_of_expierence: 13)
+
+    visit "/mechanics/#{jimbo.id}"
+
+    expect(page).to have_content(jimbo.name)
+    expect(page).to have_content(jimbo.years_of_expierence)
+  end
+
+  it "should display the names of all rides they're working on" do
+    jimbo = Mechanic.create!(name: "Jimbo", years_of_expierence: 13)
+
+    busch_gardens = Park.create!(name: "Busch Gardens", admission_price: 50)
+
+    montu = busch_gardens.rides.create!(name: "Montu", thrill_rating: 8)
+    kumba = busch_gardens.rides.create!(name: "Kumba", thrill_rating: 7)
+
+    jimbo.rides <<  montu
+    jimbo.rides <<  kumba
+
+    visit "/mechanics/#{jimbo.id}"
+
+    expect(page).to have_content("Rides Currently Working On")
+    expect(page).to have_content(montu.name)
+    expect(page).to have_content(kumba.name)
 
   end
 end
@@ -14,4 +38,3 @@ end
 # When I fill in that field with an id of an existing ride and hit submit
 # I’m taken back to that mechanics show page
 # And I see the name of that newly added ride on this mechanics show page
- 
